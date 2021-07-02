7b2d5372826896caa432f538fe16a0deb16259ae
dvogel <daniel.vogel@hallowelt.com>
2021-07-02 09:29:22 +0200

    Going on with DropdowItemList

---
+++ src/Renderer/DropdownItemList.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace MWStake\MediaWiki\Component\CommonUserInterface\Renderer;
+
+use MWStake\MediaWiki\Component\CommonUserInterface\Component\DropdownItemList as DropdownItemListComponent;
+use MWStake\MediaWiki\Component\CommonUserInterface\IComponent;
+
+class DropdownItemList extends RendererBase {
+
+	/**
+	 *
+	 * @param IComponent $component
+	 * @return bool
+	 */
+	public function canRender( IComponent $component ) : bool {
+		return $component instanceof DropdownItemListComponent;
+	}
+
+	/**
+	 * Having this public should enable client-side rendering
+	 *
+	 * @param LiteralComponent $component
+	 * @param array $subComponentNodes
+	 * @return array
+	 */
+	public function getRendererDataTreeNode( $component, $subComponentNodes ) : array {
+		$data = [
+			'id' => $component->getId(),
+			'text' => $component->getHtml()
+		];
+
+		return $data;
+	}
+
+	/**
+	 * Having this public should enable client-side rendering
+	 *
+	 * @return string
+	 */
+	public function getTemplatePathname(): string {
+		return $this->templateBasePath . '/dropdown-item-list.mustache';
+	}
+}
