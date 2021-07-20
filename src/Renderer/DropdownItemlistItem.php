2df569bc60067b5e2c634d0052509e5923d2129c
dvogel <daniel.vogel@hallowelt.com>
2021-07-20 10:10:21 +0200

    Add DropdowItemlistItem

---
+++ src/Renderer/DropdownItemlistItem.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace MWStake\MediaWiki\Component\CommonUserInterface\Renderer;
+
+use Exception;
+use MWStake\MediaWiki\Component\CommonUserInterface\IComponent;
+use MWStake\MediaWiki\Component\CommonUserInterface\IDropdownItemlistItem;
+
+class DropdownItemlistItem extends RendererBase {
+
+	/**
+	 *
+	 * @param IComponent $component
+	 * @return bool
+	 */
+	public function canRender( IComponent $component ) : bool {
+		return $component instanceof IDropdownItemlistItem;
+	}
+
+	/**
+	 * Having this public should enable client-side rendering
+	 *
+	 * @param IDropdownItemlist $component
+	 * @param array $subComponentNodes
+	 * @return array
+	 */
+	public function getRendererDataTreeNode( $component, $subComponentNodes ) : array {
+		$templateData = [];
+
+		/** @var IComponent $component */
+		if ( $component instanceof IDropdownItemlistItem ) {
+			$templateData = [
+					'list-id' => $component->getId(),
+					'body' => $subComponentNodes
+			];
+			if ( !empty( $component->getClasses() ) ) {
+				$templateData = array_merge(
+					$templateData,
+					[
+						'list-class' => implode( ' ', $component->getClasses() )
+					]
+				);
+			}
+		} else {
+			throw new Exception( "Can not extract data from " . get_class( $component ) );
+		}
+
+		return $templateData;
+	}
+
+	/**
+	 * Having this public should enable client-side rendering
+	 *
+	 * @return string
+	 */
+	public function getTemplatePathname(): string {
+		return $this->templateBasePath . '/dropdown-itemlist-item.mustache';
+	}
+}
