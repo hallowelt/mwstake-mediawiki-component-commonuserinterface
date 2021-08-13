950438b7da9898108cc5fc3e7749a2cc6e43f284
dvogel <daniel.vogel@hallowelt.com>
2021-08-13 13:57:44 +0200

    Working on SimpleMediaObject component

---
+++ src/IMediaObject.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace MWStake\MediaWiki\Component\CommonUserInterface;
+
+use Message;
+
+interface IMediaObject {
+
+	/**
+	 * @return string[]
+	 */
+	public function getContainerClasses() : array;
+
+	/**
+	 * @return string[]
+	 */
+	public function getImageClasses() : array;
+
+	/**
+	 * @return string[]
+	 */
+	public function getBodyClasses() : array;
+
+	/**
+	 *
+	 * @return Message
+	 */
+	public function getImageAltText() : Message;
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getImageSrc() : string;
+}
