b25b6dcc14a81dfd63ff359eae10fea8e3d0c9e1
miriam schlindwein <schlindwein@hallowelt.com>
2021-10-08 12:19:49 +0200

    Add DropdownIconSplitButton

---
+++ src/IDropdownIconSplitButton.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace MWStake\MediaWiki\Component\CommonUserInterface;
+
+use Message;
+
+interface IDropdownIconSplitButton extends IComponent {
+
+	/**
+	 * @return string[]
+	 */
+	public function getContainerClasses() : array;
+
+	/**
+	 * @return string[]
+	 */
+	public function getButtonGroupClasses() : array;
+
+	/**
+	 * @return string[]
+	 */
+	public function getButtonClasses() : array;
+
+	/**
+	 * @return string
+	 */
+	public function getHref() : string;
+
+	/**
+	 * @return string[]
+	 */
+	public function getSplitButtonClasses() : array;
+
+	/**
+	 * @return string[]
+	 */
+	public function getMenuClasses() : array;
+
+	/**
+	 * @return Message
+	 */
+	public function getIconClasses() : array;
+
+	/**
+	 * @return Message
+	 */
+	public function getButtonTitle() : Message;
+
+		/**
+		 * @return Message
+		 */
+	public function getSplitButtonTitle() : Message;
+
+	/**
+	 * @return Message
+	 */
+	public function getButtonAriaLabel() : Message;
+
+	/**
+	 * @return Message
+	 */
+	public function getSplitButtonAriaLabel() : Message;
+
+	/**
+	 *
+	 * @return bool
+	 */
+	public function buttonIsDisabled() : bool;
+
+	/**
+	 *
+	 * @return bool
+	 */
+	public function splitButtonIsDisabled() : bool;
+}
