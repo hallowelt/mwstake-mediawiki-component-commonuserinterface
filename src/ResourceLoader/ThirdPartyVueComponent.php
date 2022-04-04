7cd6ba73889f86ab73a6803ed1fd9812b05ed7c8
rvogel <vogel@hallowelt.biz>
2022-04-04 12:19:34 +0200

    Fix use of placeholder brackets
    ERM28224

---
+++ src/ResourceLoader/ThirdPartyVueComponent.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace MWStake\MediaWiki\Component\CommonUserInterface\ResourceLoader;
+
+use ResourceLoaderContext;
+use ResourceLoaderFileModule;
+
+class ThirdPartyVueComponent extends ResourceLoaderFileModule {
+
+	/**
+	 * @var string
+	 */
+	private $moduleExports = '';
+
+
+	/**
+	 * @inheritDoc
+	 */
+	public function __construct(
+		array $options = [],
+		$localBasePath = null,
+		$remoteBasePath = null
+	) {
+		if ( !empty( $options['module.exports'] ) ) {
+			$this->moduleExports = $options['module.exports'];
+		}
+		parent::__construct( $options, $localBasePath, $remoteBasePath );
+	}
+
+	/**
+	 * @var string
+	 */
+	private $currentFileContent = '';
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getPackageFiles( ResourceLoaderContext $context ) {
+		$package = parent::getPackageFiles( $context );
+
+		$modifiedFiles = [];
+		foreach ( $package['files'] as $filename => $file ) {
+			$modifiedFiles[$filename] = $file;
+			$this->currentFileContent = $file['content'];
+
+			$this->removeSourceMappingURL();
+			$this->maybeAddModuleExports();
+			$this->trimFileContent();
+
+			$modifiedFiles[$filename]['content'] = $this->currentFileContent;
+		}
+		$package['files'] = $modifiedFiles;
+
+		return $package;
+	}
+
+	private function removeSourceMappingURL() {
+		$this->currentFileContent = preg_replace(
+			"|//# sourceMappingURL=.*?$|s",
+			'',
+			$this->currentFileContent
+		);
+	}
+
+	private function maybeAddModuleExports() {
+		if ( !empty( $this->moduleExports ) ) {
+			$this->currentFileContent .= ";module.exports={$this->moduleExports};";
+		}
+	}
+
+	private function trimFileContent() {
+		$this->currentFileContent = trim( $this->currentFileContent );
+	}
+}
