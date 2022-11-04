b29bd3bc1ef4bce75ae404e67ea23673c490c0b9
dvogel <daniel.vogel@hallowelt.com>
2022-11-04 13:28:13 +0100

    Update phpunit test for TreeDataGenerator

--- tests/phpunit/TreeDataGeneratorTest.php
+++ tests/phpunit/TreeDataGeneratorTest.php
@@ -4,6 +4,7 @@ namespace MWStake\MediaWiki\Component\CommonUserInterface\Tests;
 
 use MediaWiki\MediaWikiServices;
 use MWStake\MediaWiki\Component\CommonUserInterface\Component\SimpleTreeTextNode;
+use MWStake\MediaWiki\Component\CommonUserInterface\Component\SimpleTreeLinkNode;
 use MWStake\MediaWiki\Component\CommonUserInterface\TreeDataGenerator;
 use PHPUnit\Framework\TestCase;
 
@@ -63,6 +64,41 @@ class TreeDataGeneratorTest extends TestCase {
 	}
 
 	protected function getExpectedData(): array {
-		return [];
+		return [
+			new SimpleTreeTextNode( [
+				'id' => 'root-node',
+				'text' => 'node-1',
+				'items' => [
+					new SimpleTreeLinkNode( [
+						'id' => 'dummy-2',
+						'text' => 'node-3',
+						'href' => 'test',
+						'items' => []
+					] ),
+					new SimpleTreeTextNode( [
+						'id' => 'dummy-3',
+						'text' => 'node-4',
+						'items' => [
+							new SimpleTreeTextNode( [
+								'id' => 'dummy-4',
+								'text' => 'node-5',
+								'items' => []
+							] ),
+						]
+					] ),
+				]
+			] ),
+			new SimpleTreeTextNode( [
+				'id' => 'dummy-5',
+				'text' => 'node-2',
+				'items' => [
+					new SimpleTreeTextNode( [
+						'id' => 'dummy-6',
+						'text' => 'node-6',
+						'items' => []
+					] ),
+				]
+			] ),
+		];
 	}
 }
