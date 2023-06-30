6f8f43908e48ac2d699c367519fcf2a39750308c
DvogelHallowelt <32163064+DvogelHallowelt@users.noreply.github.com>
2023-06-30 11:34:28 +0200

    Fix msg text of tree nodes (#32)
    ERM33091

--- src/Component/SimpleTreeLinkNode.php
+++ src/Component/SimpleTreeLinkNode.php
@@ -81,6 +81,10 @@ class SimpleTreeLinkNode extends ComponentBase implements ITreeNode, ITreeLinkNo
 	 * @return Message
 	 */
 	public function getText(): Message {
+		$msg = Message::newFromKey( $this->options['text'] );
+		if ( $msg->exists() ) {
+			return $msg;
+		}
 		return new RawMessage( $this->options['text'] );
 	}
 
@@ -88,6 +92,10 @@ class SimpleTreeLinkNode extends ComponentBase implements ITreeNode, ITreeLinkNo
 	 * @return Message
 	 */
 	public function getTitle(): Message {
+		$msg = Message::newFromKey( $this->options['title'] );
+		if ( $msg->exists() ) {
+			return $msg;
+		}
 		return new RawMessage( $this->options['title'] );
 	}
 
