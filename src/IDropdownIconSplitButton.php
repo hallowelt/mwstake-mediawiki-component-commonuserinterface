50c7b55f026e7dab33106c1dabb6991a164a1f48
Robert Vogel <1201528+osnard@users.noreply.github.com>
2023-11-28 14:06:34 +0100

    Update IDropdownIconSplitButton.php

--- src/IDropdownIconSplitButton.php
+++ src/IDropdownIconSplitButton.php
@@ -46,9 +46,9 @@ interface IDropdownIconSplitButton extends IComponent {
 	 */
 	public function getButtonTitle(): Message;
 
-		/**
-		 * @return Message
-		 */
+	/**
+	 * @return Message
+	 */
 	public function getSplitButtonTitle(): Message;
 
 	/**
@@ -67,12 +67,12 @@ interface IDropdownIconSplitButton extends IComponent {
 	 */
 	public function buttonIsDisabled(): bool;
 
-    /**
+	/**
 	 * @return string
 	 */
 	public function getButtonRel(): string;
   
-  /**
+	/**
 	 *
 	 * @return bool
 	 */
