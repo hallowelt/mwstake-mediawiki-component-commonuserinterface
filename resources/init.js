6e9ae616f029f73b88c10fc45920b64d77c252c2
hslater <hamish.slater@hallowelt.com>
2025-10-30 16:57:14 +0700

    Fix CI

--- resources/init.js
+++ resources/init.js
@@ -1,1 +1,1 @@
-var config = require( './renderer.json' ); // eslint-disable-line no-implicit-globals, no-unused-vars, no-var
+var config = require( './renderer.json' ); // eslint-disable-line no-unused-vars, no-var
